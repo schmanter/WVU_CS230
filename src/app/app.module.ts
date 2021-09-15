db32533f1ad5a9b6e96818781d24a07e280c759f
schmanter <gap12363@gmail.com>
2021-09-15 17:33:51 -0400

    split top component and some css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FeedComponent } from './feed/feed.component';
 import { BottomNavBarComponent } from './footer/bottom-nav-bar.component';
+import { SearchComponent } from './header/search.component';
 import { TopNavBarComponent} from './header/top-nav-bar.component';
+import { TopNavButtons } from './header/top-nav-buttons.component';
+import { LayoutMainComponent } from './layout/layout-main.component';
 import { SideNavBarComponent } from './sidebar/side-nav-bar.component';
 
 @NgModule({
@@ -13,7 +16,10 @@ import { SideNavBarComponent } from './sidebar/side-nav-bar.component';
     TopNavBarComponent,
     BottomNavBarComponent,
     FeedComponent,
-    SideNavBarComponent
+    SideNavBarComponent,
+    LayoutMainComponent,
+    SearchComponent,
+    TopNavButtons
   ],
   imports: [
     BrowserModule
