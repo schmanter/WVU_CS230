f53353974cb3bf1d8e28a95ffcd19d0f37fac0a4
schmanter <gap12363@gmail.com>
2021-09-15 13:10:13 -0400

    separated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FeedComponent } from './feed/feed.component';
+import { BottomNavBarComponent } from './footer/bottom-nav-bar.component';
 import { TopNavBarComponent} from './header/top-nav-bar.component';
+import { SideNavBarComponent } from './sidebar/side-nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    BottomNavBarComponent,
+    FeedComponent,
+    SideNavBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sidebar/side-nav-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-side-nav-bar',
+    templateUrl: 'side-nav-bar.component.html'
+})
+export class SideNavBarComponent {
+
+}
