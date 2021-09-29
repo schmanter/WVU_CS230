82da217f4772ac0dae7c182172b7911663c38991
schmanter <gap12363@gmail.com>
2021-09-29 13:58:34 -0400

    started lab 5

--- src/app/body/games/games-page.component.ts
+++ src/app/body/games/games-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { GamesCards } from "./games-cards-model";
+import { mock_games } from "./mock-games";
 
 @Component({
     selector: 'app-games-page',
@@ -6,5 +8,11 @@ import { Component } from "@angular/core";
     styleUrls: ["games-page.component.css"]
 })
 export class GamesPageComponent {
-    
+    games:GamesCards[] = [];
+
+    constructor () {
+        for (var games of mock_games) {
+            this.games.push(new GamesCards(games));
+        }
+    }
 }
